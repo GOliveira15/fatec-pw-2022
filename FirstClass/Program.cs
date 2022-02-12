a3d8b588fb9c8c36a587ec79c5d98add35ba9466
Gabriel Oliveira <65974597+GOliveira15@users.noreply.github.com>
2022-02-11 22:07:22 -0300

    Add files via upload

---
+++ FirstClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            if (10 < 12)
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+               Console.WriteLine("Falso");
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
+            while (true)
+            {
+
+            }
+
+            foreach (var item in collection)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
